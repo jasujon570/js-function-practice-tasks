25a5bdb1d85fd1d239d470677b51c998212ca127
sujon <jasujon570@gmail.com>
2025-08-08 21:46:01 +0600

    count total 0 in a binary string using for loop

---
+++ binary_count.js
@@ -0,0 +1,15 @@
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+
+function count_zero(binarayString){
+    let count = 0;
+    for(i = 0; i < binarayString.length; i++) {
+        if(binarayString[i] === '0'){
+          count++;
+        }
+    }
+    return count;
+}
+
+const result = count_zero('100000001100001000');
+console.log('Total 0s are in the binary string:',result);
